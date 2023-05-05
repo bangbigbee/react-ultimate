087c0e8c6f03038cbb3f6429cf7774f39787a8a2
bangbigbee <bangdtbk@gmail.com>
2023-05-05 16:22:51 +0700

    change app.js to function component, remove some css, installed bootstrap

--- src/App.js
+++ src/App.js
@@ -5,33 +5,24 @@ import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
 import React from 'react';
 
-class App extends React.Component {
+// class App extends React.Component {
 
-  render() {
-    return (
-      <div className='app-container'>
-        Hello World from BigBee Media!
-        <MyComponent></MyComponent>
-      </div>
-    )
-  }
-}
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello World from BigBee Media!
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
+//   render() {
+//     return (
+//       <div className='app-container'>
+//         Hello World from BigBee Media!
+//         <MyComponent></MyComponent>
+//       </div>
+//     )
+//   }
 // }
+
+const App = () => {
+
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  );
+}
 export default App;
